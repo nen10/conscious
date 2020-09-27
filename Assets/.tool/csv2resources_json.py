85c3955b249fed87a50bfe4db9f3ec02270184a3
nen10 <aqua5x4@i.softbank.jp>
2020-09-28 02:48:20 +0900

    write tool for convert csv to json files by 1 line

--- Assets/.tool/csv2resources_json.py
+++ Assets/.tool/csv2resources_json.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import glob
 from enum import IntEnum
 import json
@@ -13,34 +14,34 @@ class Resource(IntEnum):
     # ITEM = 5
 RESOURCE2KEYS = {
     Resource.SKILL: {
-        "input": "./json/skill.json",
+        "input": "./csv/skill.csv",
         "output": "../Resources/Json/Skill/",
         "name": "Skill",
-        "template": "./json/template_skill.json",
+        "template": "./csv/template_skill.csv",
         },
     Resource.EQUIP : {
-        "input": "./json/equip.json",
+        "input": "./csv/equip.csv",
         "output": "../Resources/Json/Equip/",
         "name": "Equip",
-        "template": "./json/template_equip.json",
+        "template": "./csv/template_equip.csv",
         },
     Resource.CHARACTOR : {
-        "input": "./json/charactor.json",
+        "input": "./csv/charactor.csv",
         "output": "../Resources/Json/Charactor/",
         "name": "Charactor",
-        "template": "./json/template_charactor.json",
+        "template": "./csv/template_charactor.csv",
         },
     Resource.PARTYSETS : {
-        "input": "./json/partySets.json",
+        "input": "./csv/partySets.csv",
         "output": "../Resources/Json/PartySets/",
         "name": "PartySets",
-        "template": "./json/template_partySets.json",
+        "template": "./csv/template_partySets.csv",
         },
     # Resource.ITEM : {
-    #   "input": "./json/item.json",
+    #   "input": "./csv/item.csv",
     #   "output": "../Resources/Json/Item/",
     #   "name": "Item",
-    #   "template": "./json/template_item.json",
+    #   "template": "./csv/template_item.csv",
     #   },
 }
 SUMMARY_PATH = "../Resources/Json/summary.json"
@@ -52,9 +53,9 @@ SUMMARY_FORMAT = OrderedDict({
     # "Item": [],
 })
 class Format:
-    STR = ""
-    INT = 0
-    FROAT = 0.0
+    STR = "str"
+    INT = "int"
+    FROAT = "float"
     STRS = ["",]
 
 FORMAT = {
@@ -106,6 +107,19 @@ FORMAT = {
     },
     # Resource.ITEM : {},
 }
+CSV_FORMAT = {
+    Resource.SKILL:
+        "file_id,name,key,req.h,req.m,req.f,req.i",
+    Resource.EQUIP: 
+        "file_id,name,role,inoperative,operative,space,self,lvr,dur.p,dur.growrateKey,\
+        HP.p,HP.growrateKey,MOV.p,MOV.growrateKey,CP.p,CP.growrateKey,DEX.p,DEX.growrateKey,\
+        RNGmin.p,RNGmin.growrateKey,RNGmax.p,RNGmax.growrateKey,SIGmin.p,SIGmin.growrateKey,SIGmax.p,SIGmax.growrateKey",
+    Resource.CHARACTOR:
+        "file_id,race,role,equip,skilltree.h,skilltree.m,skilltree.f,skilltree.i,INT,HP,MOV,CP,DEX,RNGmin,RNGmax,SIGmin,SIGmax",
+    Resource.PARTYSETS:
+        "file_id,partyid,members,skilltree.h,skilltree.m,skilltree.f,skilltree.i,lvr,lvg,lvc",
+    # Resource.ITEM: "",
+}
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -116,13 +130,52 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def json_split(res: Resource, indent: int):
-    with open(RESOURCE2KEYS[res]["input"]) as f:
-        od: OrderedDict = json.load(f, object_pairs_hook=OrderedDict)
+def csv2json_by_1_line(res: Resource, indent: int):
+    with open(RESOURCE2KEYS[res]["input"], 'r') as f:
+        data = csv.DictReader(f)
+        for row in data:
+            format_csv4json(res, row, indent)
+def format_csv4json(res: Resource, csv_1_line: OrderedDict, indent: int):
+    p = OrderedDict()
+    filename = ""
+    for k in list(csv_1_line.keys()):
+        keytree: list = k.split(".")
+        if keytree[0] == "file_id":  # skip
+            filename = (str)(csv_1_line[k])
+            continue
+        p = cast_csv2dict(FORMAT[res], csv_1_line, keytree, k, 0, p)
 
-    for k in list(od.keys()) :
-        json_dump(RESOURCE2KEYS[res]["output"] + k + ".json", od[k], indent)
-    SUMMARY_FORMAT[RESOURCE2KEYS[res]["name"]] = list(od.keys())
+        # datatype = FORMAT[res][keytree[0]]
+        # if len(keytree) > 1:
+        #     sp: OrderedDict = p.get(keytree[0], OrderedDict())
+        #     sp[keytree[1]] = (float)(csv_1_line[k]) if keytree[1] == "p" else (int)(csv_1_line[k])
+        #     p[keytree[0]] = sp
+        # elif datatype == Format.STR:
+        #     p[keytree[0]] = (str)(csv_1_line[k])
+        # elif datatype == Format.INT:
+        #     p[keytree[0]] = (int)(csv_1_line[k])
+        # elif datatype == Format.FROAT:
+        #     p[keytree[0]] = (float)(csv_1_line[k])
+        # elif datatype == Format.STRS:
+        #     p[keytree[0]] = csv_1_line[k].split(" - ")
+    
+    json_dump(RESOURCE2KEYS[res]["output"] + filename + ".json", p, indent)
+    SUMMARY_FORMAT[RESOURCE2KEYS[res]["name"]].append(filename)
+    
+def cast_csv2dict(format_dict: dict, line: OrderedDict, keytree: list, k: str, n: int, ret: OrderedDict):
+    if len(keytree) > n+1:
+        sp: OrderedDict = ret.get(keytree[n], OrderedDict())
+        ret[keytree[n]] = cast_csv2dict(format_dict[keytree[n]],line, keytree, k, n+1, ret.get(keytree[n], OrderedDict()))
+    elif format_dict[keytree[n]] == Format.STR:
+        ret[keytree[n]] = (str)(line[k])
+    elif format_dict[keytree[n]] == Format.INT:
+        ret[keytree[n]] = (int)(line[k])
+    elif format_dict[keytree[n]] == Format.FROAT:
+        ret[keytree[n]] = (float)(line[k])
+    elif format_dict[keytree[n]] == Format.STRS:
+        ret[keytree[n]] = line[k].split(" - ")
+    return ret
+
 
 def json_dump(p: str, d: OrderedDict, indent: int):
     with open(p, 'w') as f:
@@ -136,16 +189,16 @@ def remove_by_resource():
     for p in glob.glob("../Resources/Json/**/*.json.meta", recursive=True):
         if os.path.isfile(p):
             os.remove(p)
-def make_template(indent: int):
-    for res in Resource:
-        d = OrderedDict({
-            "file_id1": FORMAT[res],
-            "file_id2": FORMAT[res],
-            "file_id3": FORMAT[res],
-            "file_id4": FORMAT[res],
-            }
-        )
-        json_dump(RESOURCE2KEYS[res]["template"], d, indent)
+# def make_template(indent: int):
+#     for res in Resource:
+#         d = OrderedDict({
+#             "file_id1": FORMAT[res],
+#             "file_id2": FORMAT[res],
+#             "file_id3": FORMAT[res],
+#             "file_id4": FORMAT[res],
+#             }
+#         )
+#         json_dump(RESOURCE2KEYS[res]["template"], d, indent)
 
 def main():
     args = parse_args()
@@ -154,16 +207,16 @@ def main():
     if args.remove_json_in_resources:
         remove_by_resource()
 
-    elif args.make_template:
-        make_template(indent)
+    # elif args.make_template:
+    #     make_template(indent)
 
     elif args.resource_type == 0:
         for res in Resource:
-            json_split(res, indent)
+            csv2json_by_1_line(res, indent)
         json_dump(SUMMARY_PATH, SUMMARY_FORMAT, indent)
 
     else:
-        json_split(Resource(args.resource_type), indent)
+        csv2json_by_1_line(Resource(args.resource_type), indent)
 
 if __name__ == "__main__":
     main()
